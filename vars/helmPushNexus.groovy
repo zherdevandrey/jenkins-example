3647b725508ef94addf013268453ba7da2109570
zherdev <zherdev@mail.com>
2023-07-29 20:01:44 +0200

    commit

--- vars/helmPushNexus.groovy
+++ vars/helmPushNexus.groovy
@@ -7,8 +7,8 @@ def call(){
      )]) {
         dir('helm/') {
             sh '''
-                helmVersion = ${helm show chart helm | grep version | cut -d: -f 2 | tr -d ' \'}
-                tar -czvf helm.tgz helm
+                helmVersion = ${helm show chart helm | grep version | cut -d: -f 2 | tr -d ' '}
+                tar -czvf helm-${helmVersion}.tgz helm
                 curl -u $USER:$PASS http://localhost:8081/repository/helm/ --upload-file helm-${helmVersion}.tgz -v
             '''
         }
