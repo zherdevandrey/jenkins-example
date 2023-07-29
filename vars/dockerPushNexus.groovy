6a414d3109649e4810c7d4e4ce0c1404d346cd61
zherdev <zherdev@mail.com>
2023-07-29 11:51:00 +0200

    commit

--- vars/dockerPushNexus.groovy
+++ vars/dockerPushNexus.groovy
@@ -5,7 +5,7 @@ def call(String repoName, String artifactId, String version){
              usernameVariable: "USER",
              passwordVariable: "PASS"
      )]) {
-         sh "docker login -u '$USER' -p '$PASS' nexus:8083"
+         sh "docker login -u '$USER' -p '$PASS' nexus:8081"
      }
 
     sh "docker push ${repoName}/${artifactId}:${version}"
